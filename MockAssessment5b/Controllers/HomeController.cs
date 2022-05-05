9d1ee0087ed37c33cc507255c93bf80274e3e87b
Lonnie Johnson <lonnie.e.johnson@gmail.com>
2022-05-05 18:04:20 -0500

    MockAssessment5

---
+++ MockAssessment5b/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MockAssessment5b.Models;
+using System.Diagnostics;
+
+namespace MockAssessment5b.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult GetAge()
+        {
+            return View();
+        }
+
+        //Anytime you're sending data it is going to be a post
+        [HttpPost]
+        public IActionResult Result(Person person)
+        {
+            if (person.Age >= 21)
+            {
+                person.CanDrink = true;
+            }
+
+            if (person.Age >= 16)
+            {
+                person.CanDrive = true;
+            }
+            return View(person); 
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ MockAssessment5b/Models/Person.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MockAssessment5b.Models
+{
+    public class Person
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public int Age { get; set; }
+
+        [Required]
+        [Display(Name = "UserName")]
+        [StringLength(10, ErrorMessage = "Less than 10 characters!")]
+        public string UserName { get; set; } 
+
+        [Required]
+        public bool CanDrink { get; set; }
+
+        [Required]
+        public bool CanDrive { get; set; }
+    }
+}
